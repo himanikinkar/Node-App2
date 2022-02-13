cf11c301394a31ed8027ed63e657896551f1c83a
Himani <himanikinkar02@gmail.com>
2022-02-13 23:03:36 +0530

    commit t

--- routes/posts.js
+++ routes/posts.js
@@ -57,11 +57,34 @@ router.put("/:id/like", async (req,res) => {
         }
     }catch(err){
         res.status(500).json(err);
-
+    }
+});
+//get a post
+router.get("/:id", async (req,res)=> {
+    try{
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+});
+//get timeline posts
+router.get("/timeline", async (req,res) => {
+    let postArray = [];
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({userId : currentUser._id});
+        const friendPosts = await Promise.all(
+            currentUser.following.map(friendId =>{
+                Post.find({userId: friendId});
+            })
+        );
+        res.json(userPosts.concat(...friendPosts));
+    }catch(err){
+        res.status(500).json(err);
     }
 })
-//get a post
-//get timeline posts
 
 
 
