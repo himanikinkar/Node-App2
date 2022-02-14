2578385d17132490b60c94abd4e0c36ab90e6bff
Himani <himanikinkar02@gmail.com>
2022-02-14 22:39:38 +0530

    commit 3

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
+
 //create a post
 router.post("/", async (req,res) => {
     const newPost = new Post(req.body)
@@ -70,21 +72,20 @@ router.get("/:id", async (req,res)=> {
     }
 });
 //get timeline posts
-router.get("/timeline", async (req,res) => {
-    let postArray = [];
+router.get("/timeline/all", async (req,res) => {
     try{
         const currentUser = await User.findById(req.body.userId);
         const userPosts = await Post.find({userId : currentUser._id});
         const friendPosts = await Promise.all(
-            currentUser.following.map(friendId =>{
-                Post.find({userId: friendId});
+            currentUser.followings.map(async friendId =>{
+                return await Post.find({userId: friendId});
             })
         );
         res.json(userPosts.concat(...friendPosts));
     }catch(err){
         res.status(500).json(err);
     }
-})
+});
 
 
 
